7b1dec20ef74e2b58d4315cfdbcc16ebb41d8c56
AmrMazied <amrmazied97@icloud.com>
2025-09-28 11:10:09 +0300

    new change

--- src/app/(main)/brands/page.tsx
+++ src/app/(main)/brands/page.tsx
@@ -1,5 +1,5 @@
 import BrandsCard from '@/app/_Component/BrandsCard/BrandsCard'
-import { BrandsData, brands } from '@/types/brands.type'
+import { BrandsData } from '@/types/brands.type'
 import React from 'react'
 
 export default async function page() {
@@ -9,7 +9,7 @@ export default async function page() {
   
   
   return <>
-  <h1 className=' text-orange-600 text-2xl my-7  ml-5 font-bold  '><i className="fa-solid fa-tags"></i> BRAND</h1>
+  <h1 className=' text-orange-600 text-2xl my-7  ml-5 font-bold  '><i className="fa-solid fa-tags"></i> BRAND </h1>
     <div className='w-11/12 m-auto'>
       <div className='grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5'>
         {

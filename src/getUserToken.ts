6aa9a6e07630a479e3b86cc62b0ea9778523ec65
AmrMazied <amrmazied97@icloud.com>
2025-09-28 10:45:55 +0300

    new change

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -4,14 +4,11 @@ import { cookies } from "next/headers";
 
 
 export async function getUserToken() {
-    const TokenSession = (process.env.NODE_ENV === "production" ? '__Secure-next-auth.session-token' : 'next-auth.session-token')
+    const TokenSessionChange = (process.env.NODE_ENV === "production" ? '__Secure-next-auth.session-token' : 'next-auth.session-token')
     const cookiesData = await cookies()
-    const encryptToken = cookiesData.get(TokenSession)?.value
+    const encryptToken = cookiesData.get(TokenSessionChange)?.value
     const data = await decode({ token: encryptToken, secret: process.env.NEXTAUTH_SECRET! })
-    console.log('hhhhhhhh',cookiesData);
-    
-
-
+   
     return data?.token
     
     

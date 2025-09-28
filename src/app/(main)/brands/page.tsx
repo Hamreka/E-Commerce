382b757f9004526fa3b74f8f635dc441a41cb539
AmrMazied <amrmazied97@icloud.com>
2025-09-28 11:06:45 +0300

    new commit

--- src/app/(main)/brands/page.tsx
+++ src/app/(main)/brands/page.tsx
@@ -4,18 +4,17 @@ import React from 'react'
 
 export default async function page() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/brands`)
-
- 
-  const data = await res.json()
-  const brandList = Array.isArray(data.data) ? data.data : []
+  const data :BrandsData = await res.json()
+   const brandList = Array.isArray(data.data) ? data.data : []
+  
   
   return <>
   <h1 className=' text-orange-600 text-2xl my-7  ml-5 font-bold  '><i className="fa-solid fa-tags"></i> BRAND</h1>
     <div className='w-11/12 m-auto'>
       <div className='grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5'>
         {
-          brandList?.map((brand: any) => {
-            return <BrandsCard key={brand._id} brands={brand}/>
+          brandList.map((brands) => {
+            return <BrandsCard key={brands._id} brands={brands}/>
           })
         }
       </div>
